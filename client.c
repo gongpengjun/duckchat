9e0853beaf73fb1f472ae0645dcfd433f16e878e
Jack Ziesing <Buster@Jacks-MacBook-Pro.local>
2014-11-03 21:51:25 -0800

    final commit this sunday, added my test program client.c and server.c may want to use those for cleaner start, but currently it doesn't send messages, just pseudocode there to discuss tm

--- client.c
+++ client.c
@@ -5,73 +5,86 @@
  * ./client <server-ip>
  */
  
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <stdio.h>
+#include <sys/types.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <netdb.h>
 #include "duckchat.h"
-#define BUFLEN 64 //changed this from 512
-#define PORT 9930
+#define BUFLEN 1024
  
+int connectToSocket(char*, char*);
+int logUserIn(char*);
+void err(char*);
+
+struct addrinfo *addrAr;
+int sockfd;
+
+int main(int argc, char** argv)
+{
+    //initialize vars
+    addrAr = NULL;
+    sockfd = 0;
+    //error checking correct run input
+    if(argc != 4)
+    {
+      printf("Specify ip port username\n");
+      exit(0);
+    }
+    //call to connect socket and call to send login request to server
+    connectToSocket(argv[1], argv[2]);
+    logUserIn(argv[3]);
+
+    while(1)
+    {
+    //We want to get array of request structs  
+    //then evaluate request and have different method handle each request
+    }
+ 
+    return 0;
+}
+
 void err(char *s)
 {
     perror(s);
     exit(1);
 }
- 
-int main(int argc, char** argv)
+
+int logUserIn(char* user)
 {
-    struct sockaddr_in serv_addr;
-    int sockfd, i, slen=sizeof(serv_addr);
-    char buf[BUFLEN];
-	//adding struct
-	struct request_say message;
- 
-    if(argc != 2)
-    {
-      printf("Usage : %s <Server-IP>\n",argv[0]);
-      exit(0);
-    }
- 
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
-        err("socket");
- 
-    bzero(&serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    if (inet_aton(argv[1], &serv_addr.sin_addr)==0)
-    {
-        fprintf(stderr, "inet_aton() failed\n");
-        exit(1);
-    }
- 
-    while(1)
-    {
-        printf("\nEnter data to send(Type exit and press enter to exit) : ");
-        scanf("%[^\n]",buf);
-        getchar();
-        if(strcmp(buf,"exit") == 0)
-          exit(0);
-	//adding struct
-	printf("before set\n");
-	message.req_type = REQ_SAY;
-	printf("before strcpy\n");
-	strcpy(message.req_channel, "default\n");
-	strcpy(message.req_text, buf);
- 
-        //if (sendto(sockfd, buf, BUFLEN, 0, (struct
-	printf("attempting to send message\n");
-	if (sendto(sockfd, &message, sizeof(message), 0, (struct
- sockaddr*)&serv_addr, slen)==-1)
-            err("sendto()");
-    }
- 
-    close(sockfd);
-    return 0;
+    //send login request to server
+    return true;
 }
- 
-//</server-ip>
+
+int connectToSocket(char* ip, char* port)
+{
+    struct addrinfo addressTmp;
+    struct addrinfo *tmpAdAr;
+    memset(&addressTmp, 0, sizeof addressTmp);
+    addressTmp.ai_family = AF_INET;
+    addressTmp.ai_socktype = SOCK_DGRAM;
+    int check = 0;
+    if((check = getaddrinfo(ip, port, &addressTmp, &addrAr))!= 0)
+    {
+        err("Client : getaddrinfo() NOT successful\n");
+        return false;
+    }
+    for(tmpAdAr = addrAr; tmpAdAr != NULL; tmpAdAr = tmpAdAr->ai_next)
+    {
+        sockfd = socket(tmpAdAr->ai_family, tmpAdAr->ai_socktype, tmpAdAr->ai_protocol);
+        if(sockfd == -1)
+        {
+            err("Client : socket() NOT successful\n");
+            continue;
+        }
+        break;
+    }
+    addrAr = tmpAdAr;
+    
+    printf("socket and bind successful! \n");
+    return true;
+}
--- server.c
+++ server.c
@@ -16,57 +16,44 @@
 #include <stdbool.h>
 #include <netdb.h>
 #include "duckchat.h"
-#define BUFLEN 512
-#define PORT 9930
+#define BUFLEN 1024
+
+socklen_t fromlen;
+struct sockaddr recAddr;
+int sockfd;
 
 int connectToSocket(char*, char*);
+void err(char*);
 
 struct addrinfo *addrAr;
  
+
+int main(int argc, char **argv)
+{
+    addrAr = NULL;
+    sockfd = 0;
+    char buf[BUFLEN];
+    connectToSocket(argv[1], argv[2]);
+    while(1)
+    {
+        struct request *requests = (struct request*) malloc(sizeof (struct request) + BUFLEN);  
+        int bal = 0;
+        bal = recvfrom(sockfd, buf, sizeof buf, 0, &recAddr, &fromlen);
+        if(bal > 0) 
+        {
+            printf("recv()'d %d bytes of data in buf\n", bal);    
+        }
+        
+        
+    }
+    //close(sockfd);
+    return 0;
+}
+
 void err(char *str)
 {
     perror(str);
     exit(1);
-}
- 
-int main(int argc, char **argv)
-{
-    addrAr = NULL;
-    struct sockaddr_in my_addr, cli_addr;
-    int sockfd, i;
-    socklen_t slen=sizeof(cli_addr);
-    char buf[BUFLEN];
-    
-	//adding struct
-	struct request_say message;
-    connectToSocket(argv[1], argv[2]);
-    // if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
-    //   err("socket");
-    // else
-    //   printf("Server : Socket() successful\n");
- 
-    // bzero(&my_addr, sizeof(my_addr));
-    // my_addr.sin_family = AF_INET;
-    // my_addr.sin_port = htons(PORT);
-    // my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-     
-    // if (bind(sockfd, (struct sockaddr* ) &my_addr, sizeof(my_addr))==-1)
-    //   err("bind");
-    // else
-    //   printf("Server : bind() successful\n");
- 
-    while(1)
-    {
-        //if (recvfrom(sockfd, buf, BUFLEN, 0, (struct
-	// if (recvfrom(sockfd, &message, sizeof(message), 0, (struct
- // sockaddr*)&cli_addr, &slen)==-1)
- //            err("recvfrom()");
- //        printf("Received packet from %s:%d\nData: %s\n\n",
- //               inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port), message.req_text);
-     }
- 
-    //close(sockfd);
-    return 0;
 }
 
 int connectToSocket(char* ip, char* port)
@@ -76,18 +63,18 @@ int connectToSocket(char* ip, char* port)
     addressTmp.ai_family = AF_INET;
     addressTmp.ai_socktype = SOCK_DGRAM;
     addressTmp.ai_flags = AI_PASSIVE;
-    int check, secCheck = 0;
+    int check = 0;
     if((check = getaddrinfo(ip, port, &addressTmp, &addrAr))!= 0)
     {
         err("Server : getaddrinfo() NOT successful\n");
         return false;
     }
-    if((check = socket(addrAr->ai_family, addrAr->ai_socktype, addrAr->ai_protocol)) == -1)
+    if((sockfd = socket(addrAr->ai_family, addrAr->ai_socktype, addrAr->ai_protocol)) == -1)
     {
         err("Server : socket() NOT successful\n");
         return false;
     }
-    if(bind(check, addrAr->ai_addr, addrAr->ai_addrlen) == -1)
+    if(bind(sockfd, addrAr->ai_addr, addrAr->ai_addrlen) == -1)
     {
         err("Server : bind() NOT successful\n");
         return false;
