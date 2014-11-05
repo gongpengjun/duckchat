4b2cff5e96e778659c563023a71e72e3a7a6aeed
Jack Ziesing <Buster@128-223-223-123-wireless.uoregon.edu>
2014-11-04 16:43:00 -0800

    hehe:wq

--- server.cpp
+++ server.cpp
@@ -63,7 +63,7 @@ int main(int argc, char **argv)
         // requests = (struct request*) malloc(sizeof (struct request) + BUFLEN); 
         struct request requests;  
         int bal = 0;
-        bal = recvfrom(sockfd, &requests, sizeof requests, 0, &recAddr, &fromlen);
+        bal = recvfrom(sockfd, &requests, (sizeof (struct request) + 1024), 0, &recAddr, &fromlen);
         if(bal > 0) {
             printf("recv()'d %d bytes of data in buf\n", bal);
             readRequestType(&requests, bal);       
