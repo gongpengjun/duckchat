4adf9b23fc4d8e00ecedb2411a2b409e6498cafe
Jack Ziesing <Buster@128-223-223-84-wireless.uoregon.edu>
2014-11-07 15:05:00 -0800

    hshs

--- server.cpp
+++ server.cpp
@@ -163,8 +163,8 @@ int sayReq(struct request_say *rs)
         strncpy(msg->txt_text, message.c_str(), SAY_MAX);
         strncpy(msg->txt_channel, channel.c_str(), CHANNEL_MAX);
         //send message
-	    //int size = sizeof(struct sockaddr_in);
-        int res= sendto(sockfd, msg, sizeof(struct text_say), 0, &(address->sin_addr), sizeof(struct text_say));
+	    int size = sizeof(struct sockaddr_in);
+        int res= sendto(sockfd, msg, sizeof(struct text_say), 0, &(address->sin_addr), size);
         if (res == -1) {
             cout << "sendto very badd \n";
             return -1;
