85678884ea9bbd68e27b687d93782fb72d8b94c6
wsorenso <wsorenso@uoregon.edu>
2014-11-07 11:43:03 -0800

    fixed select bullshit i think

--- client.c
+++ client.c
@@ -9,17 +9,21 @@
 #include <sys/types.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <netdb.h>
+#include <fcntl.h>
 #include "duckchat.h"
 #define BUFLEN 1024
 #define STDIN 0
  
 int connectToSocket(char*, char*);
+//int isReadable(sd, int*, int timeOut);
 int logUserIn(char*);
+void sendReqs();
 void err(char*);
 
 struct addrinfo *addrAr;
@@ -28,11 +32,9 @@ int sockfd;
 int main(int argc, char** argv)
 {
     //initialize vars
+	char buf[BUFLEN];
     addrAr = NULL;
     sockfd = 0;
-	fd_set readfds;
-	FD_ZERO(&readfds);
-	FD_SET(STDIN, &readfds);
     //error checking correct run input
     if(argc != 4)
     {
@@ -43,23 +45,42 @@ int main(int argc, char** argv)
     connectToSocket(argv[1], argv[2]);
     logUserIn(argv[3]);
 	printf("argv 1: %s argv 2: %s argv 3: %s\n", argv[1], argv[2], argv[3]);
-	FD_SET(sockfd, &readfds);//add our socket to our set of files to read from
+	//FD_SET(sockfd, &readfds);//add our socket to our set of files to read from
+
+	struct text_say response;//for testing
 
     while(1)
     {
-    //We want to get array of request structs  
-    //then evaluate request and have different method handle each request
-	//call select to read from stdin and sock at same time
-	if(select(sockfd+1, &readfds, NULL, NULL, NULL) == -1){
+    	//We want to get array of request structs  
+    	//then evaluate request and have different method handle each request
+	fd_set readfds;
+	struct timeval tv;
+	tv.tv_sec = 1;
+	tv.tv_usec = 0;
+	FD_ZERO(&readfds);//clean set
+	FD_SET(STDIN, &readfds);//add stdin
+
+	//call select to read from stdin if there is input available
+	if(select(STDIN+1, &readfds, NULL, NULL, &tv) == -1){
 		perror("select\n");
 		exit(4);
 	}//end select
-	
-	
-    }
- 
-    return 0;
-}
+
+	//printf("\nEnter data to send(Type exit and press enter to exit) :");
+	if(FD_ISSET(STDIN, &readfds))
+	{
+		FD_CLR(STDIN, &readfds);
+		scanf("%[^\n]",buf);
+		getchar();
+		if(strcmp(buf,"exit") == 0)
+		  exit(0);
+	}
+        
+	if(recvfrom(sockfd, &response, sizeof(response), 0,(struct sockaddr*)&addrAr->ai_addr, &addrAr->ai_addrlen)>0)//if we recieve somthing, print it.
+	printf("Response: %s\n", response.txt_text);
+    
+	}
+}//end main
 
 void err(char *s)
 {
@@ -108,6 +129,7 @@ int connectToSocket(char* ip, char* port)
     for(tmpAdAr = addrAr; tmpAdAr != NULL; tmpAdAr = tmpAdAr->ai_next)
     {
         sockfd = socket(tmpAdAr->ai_family, tmpAdAr->ai_socktype, tmpAdAr->ai_protocol);
+	//sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
         if(sockfd == -1)
         {
             err("Client : socket() NOT successful\n");
@@ -116,7 +138,32 @@ int connectToSocket(char* ip, char* port)
         break;
     }
     addrAr = tmpAdAr;
-    
+    /*int flags = fcntl(sockfd, F_GETFL);
+    flags |= O_NONBLOCK;
+    fcntl(sockfd, F_SETFL, flags);*/
+    fcntl(sockfd, F_SETFL, O_NONBLOCK);//set socket to be non-blocking
     printf("socket and bind successful! \n");
     return true;
+}
+
+int isReadable(int sd, int* error, int timeOut)
+{
+	fd_set socketReadSet;
+	FD_ZERO(&socketReadSet);
+	FD_SET(sd,&socketReadSet);
+	struct timeval tv;
+	if (timeOut){
+		tv.tv_sec  = timeOut / 1000;
+		tv.tv_usec = (timeOut % 1000) * 1000;
+	}
+	else {
+		tv.tv_sec  = 0;
+		tv.tv_usec = 0;
+	} // if
+	if (select(sd+1,&socketReadSet,0,0,&tv) == -1) {//SOCKET_ERROR
+		*error = 1;
+		return 0;
+	} // if
+	*error = 0;
+	return FD_ISSET(sd,&socketReadSet) != 0;
 }
