c600a9c2220bf8b4348141918d24a1df492dcc2b
Jack Ziesing <Buster@128-223-223-84-wireless.uoregon.edu>
2014-11-07 15:30:32 -0800

    akakak

--- server.cpp
+++ server.cpp
@@ -163,7 +163,7 @@ int sayReq(struct request_say *rs)
         strncpy(msg->txt_text, message.c_str(), SAY_MAX);
         strncpy(msg->txt_channel, channel.c_str(), CHANNEL_MAX);
         //send message
-	    size_t size = sizeof(struct sockaddr);
+	    size_t size = sizeof(struct sockaddr_in*);
         int res= sendto(sockfd, msg, sizeof(struct text_say), 0, address, size);
         if (res == -1) {
             cout << "sendto very badd \n";
