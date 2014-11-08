64bd442430ae0213776718083e7b06af49c80af9
Jack Ziesing <Buster@128-223-223-84-wireless.uoregon.edu>
2014-11-07 18:06:29 -0800

    hellooo

--- server.cpp
+++ server.cpp
@@ -192,6 +192,7 @@ int sayReq(struct request_say *rs)
         //get address of current user
         struct sockaddr_in address;
         string ad = userToAddr[tmpU[i]];
+        cout << ad << " POOP that is addeess\n";
         char *s= (char*) malloc(sizeof(char)*BUFLEN);
         //move ad to t (address)
         strncpy(s, ad.c_str(), strlen(ad.c_str()));
