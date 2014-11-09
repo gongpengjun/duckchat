ea96f1d363c44c6275c31a8ec43591042a438dca
Jack Ziesing <Buster@Jacks-MacBook-Pro.local>
2014-11-08 18:51:27 -0800

    commem

--- serverS.cpp
+++ serverS.cpp
@@ -67,10 +67,10 @@ int main(int argc, char **argv)
             requests = (request*) buf;
             cout << "do I get herer??? \n";
             readRequestType(requests, bal);  
-            map<string,struct sockaddr_in>::iterator pit;
-            for(pit = userToAddrStrct.begin(); pit!= userToAddrStrct.end(); pit++) {
-                cout << "users in struct " << pit->first << " \n";
-            }
+            // map<string,struct sockaddr_in>::iterator pit;
+            // for(pit = userToAddrStrct.begin(); pit!= userToAddrStrct.end(); pit++) {
+            //     cout << "users in struct " << pit->first << " \n";
+            // }
         } 
 
        //requests = NULL;
